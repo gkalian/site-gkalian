347e4ab47a461453826e5baf4982d05ad3bda6a1
gkalian <greatkalian@gmail.com>
2025-08-07 10:31:26 +0300

    fix: correct vite settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,32 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'node:url'
+import path from 'path'
 
 export default defineConfig({
+  base: "",
+  publicDir: path.resolve(__dirname, 'public'),
+  root: path.resolve(__dirname),
   plugins: [react()],
+  define: { 'process.env': {} },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+    extensions: [
+      '.js',
+      '.json',
+      '.jsx',
+      '.mjs',
+      '.ts',
+      '.tsx',
+      '.vue',
+    ],
+  },
   server: {
-    port: 3000,
-    open: true,
+    port: 3200,
   },
   build: {
-    outDir: 'build',
-    sourcemap: true,
+    chunkSizeWarningLimit: 1500,
   },
-});
+})
