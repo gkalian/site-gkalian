b9c37675b925a050bac34945662135e6dcf12ae7
gkalian <greatkalian@gmail.com>
2025-08-19 18:31:42 +0300

    fix: update config

--- .releaserc.js
+++ .releaserc.js
@@ -17,11 +17,36 @@ module.exports = {
       }
     ],
     [
+      '@semantic-release/exec',
+      {
+        prepareCmd: `
+          # Install yq if missed
+          if ! command -v yq &> /dev/null; then
+            wget -qO /tmp/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
+            chmod +x /tmp/yq
+            YQ_CMD="/tmp/yq"
+          else
+            YQ_CMD="yq"
+          fi
+          
+          #  Update Chart.yaml with yq
+          $YQ_CMD eval '.version = "\${nextRelease.version}"' -i helm/Chart.yaml
+          $YQ_CMD eval '.appVersion = "\${nextRelease.version}"' -i helm/Chart.yaml
+          `
+      }
+    ],
+    [
       '@semantic-release/git',
       {
-        assets: ['package.json', 'package-lock.json', 'CHANGELOG.md'],
+        assets: [
+          'package.json', 
+          'package-lock.json', 
+          'CHANGELOG.md',
+          'helm/Chart.yaml'
+        ],
         message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
       }
-    ]
+    ],
+    '@semantic-release/github'
   ]
 };
